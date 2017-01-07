ff5b42cc03401a09ba4dae29c51b666110cf1fff
akaliwod <arkadiusz.kaliwoda@gmail.com>
2017-01-07 21:41:55 +0100

    with project and openrc

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/ansible/playbooks --privileged=true --name ansible index.docker.io/codrake/ansible:2.2 $1
+#docker run --rm -it -v $(pwd):/ansible/playbooks --privileged=true --name ansible index.docker.io/codrake/ansible:2.2 $1
+
+docker run --rm -it --privileged=true \
+        --name ansible \
+        -v /root/demo/ansible/ansible-openstack:/ansible/playbooks \
+        -v /var/images/:/images \
+        -v /root/openstack-configs/:/openstack-configs \
+        index.docker.io/codrake/ansible:2.2 $1
+
